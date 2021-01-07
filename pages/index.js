452d26882b2bd7bda6d73033911ae94e3f77ca7d
Wayne Huang <wayne@craftedorigin.com>
2021-01-07 12:22:41 -0800

    Removes unused pages and components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
 
 export default function Index() {
   return (
@@ -13,11 +9,6 @@ export default function Index() {
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
