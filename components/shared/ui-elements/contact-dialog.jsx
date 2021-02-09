24cc169d07b3f20382df4b92f856cb7c2906df50
Wayne Huang <wayne@craftedorigin.com>
2021-02-08 20:17:10 -0800

    CU-may1d8: Uses Formik to create basic form with validation
    * Validation schema with Yup

--- components/shared/ui-elements/contact-dialog.jsx
+++ components/shared/ui-elements/contact-dialog.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { email, object, string } from 'yup';
 import {
+  Button,
   TextField,
   Dialog,
   DialogActions,
@@ -7,9 +9,21 @@ import {
   DialogContentText,
   DialogTitle,
 } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
 
 import ClientOnlyPortal from '@/components/shared/layouts/client-only-portal';
+
+//* Names are hashed so bots cannot freely guess them.
+const formInfo = {
+  name: { key: 'nameaert', value: '' },
+  email: { key: 'emailiuhh', value: '' },
+  message: { key: 'messageujfk', value: '' },
+};
+
+const initialValues = {
+  [formInfo.name.key]: formInfo.name.value,
+  [formInfo.email.key]: formInfo.email.value,
+  [formInfo.message.key]: formInfo.message.value,
+};
 
 export default function ContactDialog(props) {
   const { isOpenContact, handleContactClick } = props;
@@ -17,7 +31,7 @@ export default function ContactDialog(props) {
   return (
     <ClientOnlyPortal selector="#contact-hook">
       <Dialog
-        open={isOpenContact}
+        open={true}
         onClose={() => handleContactClick(false)}
         aria-labelledby="form-dialog-title"
       >
@@ -27,14 +41,68 @@ export default function ContactDialog(props) {
             To subscribe to this website, please enter your email address here.
             We will send updates occasionally.
           </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
+          <Formik
+            validationSchema={object({
+              [formInfo.name.key]: string()
+                .required('Name is required.')
+                .min(2)
+                .max(100),
+              [formInfo.email.key]: string()
+                .required('Email is required.')
+                .email('Must be a valid email.'),
+              [formInfo.message.key]: string()
+                .required('Message is required.')
+                .min(20)
+                .max(100),
+            })}
+            initialValues={initialValues}
+            onSubmit={(values, formikHelpers) => {}}
+          >
+            {({ values, errors, isSubmitting, isValidating, touched }) => (
+              <Form noValidate autoComplete="off">
+                <Field
+                  name={formInfo.name.key}
+                  as={TextField}
+                  label="Name"
+                  fullWidth
+                  variant="outlined"
+                  margin="normal"
+                  error={
+                    touched[formInfo.name.key] && !!errors[formInfo.name.key]
+                  }
+                  helperText={<ErrorMessage name={formInfo.name.key} />}
+                />
+                <Field
+                  name={formInfo.email.key}
+                  as={TextField}
+                  label="Email"
+                  fullWidth
+                  variant="outlined"
+                  margin="normal"
+                  error={
+                    touched[formInfo.email.key] && !!errors[formInfo.email.key]
+                  }
+                  helperText={<ErrorMessage name={formInfo.email.key} />}
+                />
+                <Field
+                  name={formInfo.message.key}
+                  as={TextField}
+                  label="Message"
+                  multiline
+                  rows="4"
+                  rowsMax="4"
+                  fullWidth
+                  variant="outlined"
+                  margin="normal"
+                  error={
+                    touched[formInfo.message.key] &&
+                    !!errors[formInfo.message.key]
+                  }
+                  helperText={<ErrorMessage name={formInfo.message.key} />}
+                />
+              </Form>
+            )}
+          </Formik>
         </DialogContent>
         <DialogActions>
           <Button onClick={() => handleContactClick(false)} color="primary">
