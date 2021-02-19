4c5a3fc6dd15ed6b385d0b4bc4cf67ee68c5b0c2
Wayne Huang <wayne@craftedorigin.com>
2021-02-19 13:06:04 -0800

    CU-mb0f7k: Adds favicon sizes

--- components/shared/meta.js
+++ components/shared/meta.js
@@ -1,24 +1,26 @@
 import Head from 'next/head';
 
+import { HOME_OG_IMAGE_URL } from '@/lib/constants';
+
 export default function Meta() {
   return (
     <Head>
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href="/favicon/apple-touch-icon.svg"
       />
       <link
         rel="icon"
-        type="image/png"
+        type="image/svg"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href="/favicon/favicon-32x32.svg"
       />
       <link
         rel="icon"
-        type="image/png"
+        type="image/svg"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href="/favicon/favicon-16x16.svg"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
@@ -26,13 +28,12 @@ export default function Meta() {
         href="/favicon/safari-pinned-tab.svg"
         color="#000000"
       />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon/favicon.svg" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta name="description" content={`Ember Navarro's UI UX portfolio`} />
-      {/* <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
+      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
   );
 }
